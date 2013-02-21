7ea472af6c7235eed556fd1fcb241a4aa3147ce3
Joseph Rollinson <jtrollinson@gmail.com>
2013-02-21 12:16:29 -0500

    first commit

---
+++ GP2D02.h
@@ -0,0 +1,38 @@
+/*
+  GP2D02.h - library for the Sharp GP2D02 infrared sensor
+  Created by Joseph Rollinson, 4 November 2011
+  Public Domain
+*/
+
+#ifndef GP2D02_h
+#define GP2D02_h
+
+#include "WProgram.h"
+
+/* A simple object that represents a single GP2D02 sensor */
+class GP2D02
+{
+  public:
+    GP2D02(int pinVin, int pinVout);
+    byte getRawRange(); 
+  private:
+    int _pinVin;
+    int _pinVout;
+};
+
+/* An object that represents multiple GP2D02. 
+   Much more efficient than simply using multiple GP2D02 classes. */
+class nGP2D02
+{
+	public:
+		nGP2D02(int pinVins[], int pinVouts[],int nSensors);
+		void getRawRanges(byte* ranges); 
+		byte getRawRange(int nSensor);
+	private:
+		int _pinVins[6]; //max length due to size of arduino
+		int _pinVouts[6];
+		int _nSensors;
+             
+
+};
+#endif
---
+++ README.txt
@@ -0,0 +1,13 @@
+This is for interfacing with Sharp GP2D02 infrared sensors with an arduino. 
+
+TO INSTALL:
+	If you haven't already, create a directory called 'libraries' in your sketchbook directory. Copy the GP2D02 folder into that directory.
+
+This software is free to use in your own software, but please do not steal it as your own.  Simply acknowledge me.  Thanks a lot.
+
+If you have any questions, concerns, improvements, please let me know at JTRollinson@gmail.com. 
+
+Thanks and Enjoy!
+
+Joseph Rollinson  
+
